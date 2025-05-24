a9371f204f93df4d22f87abfcabeaa0737954f2f
fitronansori <fitronansori7@gmail.com>
2025-05-24 16:56:11 +0800

    add navigation links data

---
+++ src/constants/data.ts
@@ -0,0 +1,17 @@
+export const nav_links = [
+  {
+    id: 1,
+    title: "Beranda",
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "Kursus",
+    href: "/courses",
+  },
+  {
+    id: 3,
+    title: "Instruktur",
+    href: "/instructors",
+  },
+];
