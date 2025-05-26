1e43de956bd52142d3b60fae63f9d9611ed70fd6
fitronansori <fitronansori7@gmail.com>
2025-05-26 21:56:33 +0800

    refactor: replace webhook verification logic with svix implementation for user events

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,46 +1,76 @@
 import { prisma } from "@/lib/prisma";
-import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { WebhookEvent } from "@clerk/nextjs/webhooks";
+import { headers } from "next/headers";
+import { Webhook } from "svix";
 
 export async function POST(req: Request) {
   try {
-    const evt = await verifyWebhook(req);
+    const secret = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
 
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
+    if (!secret) return new Response("Missing secret", { status: 500 });
 
-    switch (eventType) {
-      case "user.created":
-        await prisma.user.create({
-          data: {
-            clerkId: id,
-            name: evt.data.first_name + " " + evt.data.last_name,
-            username: evt.data.username,
-            email: evt.data.email_addresses[0].email_address,
-            imageUrl: evt.data.image_url || "",
-            role: evt.data.public_metadata.role || "user",
-          },
-        });
+    const wh = new Webhook(secret);
+    const body = await req.text();
+    const headerPayload = await headers();
 
-      case "user.updated":
-        await prisma.user.update({
-          where: { clerkId: id },
-          data: {
-            name: evt.data.first_name + " " + evt.data.last_name,
-            username: evt.data.username,
-            email: evt.data.email_addresses[0].email_address,
-            imageUrl: evt.data.image_url || "",
-            role: evt.data.public_metadata.role || "user",
-          },
-        });
+    const event = wh.verify(body, {
+      "svix-id": headerPayload.get("svix-id")!,
+      "svix-timestamp": headerPayload.get("svix-timestamp")!,
+      "svix-signature": headerPayload.get("svix-signature")!,
+    }) as WebhookEvent;
 
-      case "user.deleted":
-        await prisma.user.delete({
-          where: { clerkId: id },
-        });
+    if (event.type === "user.created") {
+      const {
+        id,
+        email_addresses,
+        first_name,
+        last_name,
+        username,
+        image_url,
+        public_metadata,
+      } = event.data;
 
-        break;
+      await prisma.user.create({
+        data: {
+          clerkId: id,
+          name: `${first_name} ${last_name}`,
+          username: username,
+          email: email_addresses[0].email_address,
+          imageUrl: image_url || "",
+          role: public_metadata.role || "user",
+        },
+      });
+    }
+
+    if (event.type === "user.updated") {
+      const {
+        id,
+        email_addresses,
+        first_name,
+        last_name,
+        username,
+        image_url,
+        public_metadata,
+      } = event.data;
+
+      await prisma.user.update({
+        where: { clerkId: id },
+        data: {
+          name: `${first_name} ${last_name}`,
+          username: username,
+          email: email_addresses[0].email_address,
+          imageUrl: image_url || "",
+          role: public_metadata.role || "user",
+        },
+      });
+    }
+
+    if (event.type === "user.deleted") {
+      const { id } = event.data;
+
+      await prisma.user.delete({
+        where: { clerkId: id },
+      });
     }
 
     return new Response("Webhook received", { status: 200 });
