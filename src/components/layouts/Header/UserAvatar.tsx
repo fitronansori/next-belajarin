592918f334de51c4375240482ace583ddad091a0
fitronansori <fitronansori7@gmail.com>
2025-05-27 12:09:24 +0800

    feat: enhance UserAvatar component with dashboard link and logout functionality

--- src/components/layouts/Header/UserAvatar.tsx
+++ src/components/layouts/Header/UserAvatar.tsx
@@ -10,6 +10,8 @@ import {
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
+import { SignOutButton } from "@clerk/nextjs";
+import { LayoutDashboard, LogOut } from "lucide-react";
 
 type UserAvatarProps = {
   name?: string;
@@ -44,10 +46,16 @@ const UserAvatar = ({ name, image, role }: UserAvatarProps) => {
 
         <DropdownMenuItem asChild>
           <Link href={dashboardLink} passHref>
-            Dashboard
+            <LayoutDashboard /> Dashboard
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem>Log Out</DropdownMenuItem>
+        <DropdownMenuItem className="text-red-500" asChild>
+          <SignOutButton>
+            <div className="w-full">
+              <LogOut className="text-red-500" /> Keluar
+            </div>
+          </SignOutButton>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
